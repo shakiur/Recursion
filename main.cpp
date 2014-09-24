f45cc7fda4efeabf42148d6f6fa967ae59d6a8e8
Shakiur <shakiur@gmail.com>
2014-09-24 17:37:58 -0400

    Add incomplete function to find all permutations of string

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@ using namespace std;
 // Find integer in array using binary search method
 int binarySearch(int[], int, int, int);
 
-int main() {
+// Print all permutations of string
+void printPermutations(string);
 
+void calcPermutations(string, string, int, bool[], int);
+
+int main() {
+	printPermutations("abcd");
 }
 
 int binarySearch(int array[], int lower, int upper, int target) {
@@ -22,4 +27,51 @@ int binarySearch(int array[], int lower, int upper, int target) {
 	else 
 		return binarySearch(array, lower, mid-1, target);
 	
+}
+
+void printPermutations(string str) {
+	int str_len = str.length();
+	string str_perm;
+
+	bool hold_arr[str_len];
+
+	// Set array to false
+	for(int i = 0; i < str_len; i++) {
+		hold_arr[i] = false;
+	}
+
+	calcPermutations(str, str_perm, str_len, hold_arr, 0);
+
+}
+
+void calcPermutations(	string str, 
+						string str_perm, 
+						int str_len, 
+						bool hold_array[], 
+						int place) {
+
+	if(place == str_len){
+		cout << str_perm << endl;
+		str_perm = "";
+		
+	}
+	else {
+
+		for(int i = 0; i < str_len; i++) {
+		
+			if(!hold_array[i]) {
+
+				str_perm += str[i];
+				hold_array[i] = true;
+				calcPermutations(str, str_perm, 
+								 str_len, hold_array,
+								 place+1);
+				hold_array[i] = false;
+				
+			}
+
+		}
+
+	}
+
 }
